12fc0fb2edc2f7c95e83b9e3097449336869be02
BushiJhon <1172905107@qq.com>
2020-05-31 17:40:50 +0800

    App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Banner from "./components/Banner";
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Banner/>
+            </div>
+    );
+  }
+}
+
+export default App;
